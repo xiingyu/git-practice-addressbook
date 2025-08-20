682ce087e1e82ca4b0f788c287f0a06eff2ad0e6
Won-Jongho <jhwon1009@naver.com>
2025-08-20 10:44:25 +0900

    update header file for global variables

--- addContact/addContact.h
+++ addContact/addContact.h
@@ -1,2 +1,14 @@
-#include "main.h"
+#ifndef ADDCONTACT_H
+#define ADDCONTACT_H
+
+#include <vector>
+#include "contact.h"
+
+using namespace std;
+
+extern vector<Contact> contacts;
+extern nextId;
+
 void addContact();
+
+#endif // ADDCONTACT_H
