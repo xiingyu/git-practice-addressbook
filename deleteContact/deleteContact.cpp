4f01b03fa759269acd2ca6656db5d01fcb45473f
SKH_Gasan <tls3162@naver.com>
2025-08-20 10:32:12 +0900

    deleteContact's contants upload

--- deleteContact/deleteContact.cpp
+++ deleteContact/deleteContact.cpp
@@ -1,6 +1,18 @@
 #include "deleteContact.hpp"
 
-void deleteContact() {
-	int id;
-	cout << "삭제할 연락처 ID: ";
+void deleteContact(vector<Contact> contacts) {
+    int id;
+    cout << "삭제할 연락처 ID: ";
+    cin >> id;
+
+    auto it = remove_if(contacts.begin(), contacts.end(),
+                        [id](Contact &c) { return c.id == id; });
+
+    if (it != contacts.end()) {
+        contacts.erase(it, contacts.end());
+        cout << "연락처가 삭제되었습니다.\n";
+    } else {
+        cout << "해당 ID의 연락처가 없습니다.\n";
+    }
+
 }
--- deleteContact/deleteContact.hpp
+++ deleteContact/deleteContact.hpp
@@ -1,4 +1,5 @@
 #include <iostram>
 #include <vector>
+#include <../main.hpp>
 using namepsace std;
 
