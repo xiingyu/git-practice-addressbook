dcbdc76843724c144c2e931c787a01c05bb80d01
mumumumu52 <haneul4019@gmail.com>
2025-08-20 11:59:52 +0900

    Upload find3 again

---
+++ findContact/findContact.hpp
@@ -0,0 +1,14 @@
+#ifndef FIND_CONTACT_H
+#define FIND_CONTACT_H
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../contact.hpp"
+using namespace std;
+
+extern std::vector<Contact> contacts;
+extern int nextId;
+
+void findContact();
+
+#endif
