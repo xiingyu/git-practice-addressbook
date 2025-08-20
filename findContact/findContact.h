b1e81120239ce22778d78c4036c584609a6d16e9
mumumumu52 <haneul4019@gmail.com>
2025-08-20 10:53:18 +0900

    Upload findContact2

--- findContact/findContact.h
+++ findContact/findContact.h
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <vector>
 #ifndef FIND_CONTACT_H
 #define FIND_CONTACT_H
+#include <iostream>
+#include <vector>
 #include "../contact.h"
 
 extern std::vector<Contact> contacts;
